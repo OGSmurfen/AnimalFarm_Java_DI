f4309f6aa0bfd2e7c7461a05035f3adad2cd60e0
Ilia Chakarov <114438460+OGSmurfen@users.noreply.github.com>
2024-06-07 12:24:54 +0300

    2nd

--- AnimalFarm/src/Main.java
+++ AnimalFarm/src/Main.java
@@ -2,6 +2,6 @@ import farm.Building;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to farm!");
     }
 }
